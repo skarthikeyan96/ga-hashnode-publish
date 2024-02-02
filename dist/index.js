eeb4c9e3f7f0970a36cc9289a80a475019cfd334
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2024-02-02 17:26:35 +0530

    whitelist files

--- dist/index.js
+++ dist/index.js
@@ -41,14 +41,17 @@ const run = async () => {
             for (const file of markdownFiles) {
                 const filePath = file.filename;
                 console.log("filePath", filePath);
-                const fileContentResponse = await axios_1.default.get(`https://raw.githubusercontent.com/skarthikeyan96/ga-hashnode-publish/${commitHash}/${customBlogPath}${filePath}`);
-                if (fileContentResponse.status === 200) {
-                    const fileContent = fileContentResponse.data;
-                    console.log("fileContent", fileContent);
-                    parseMdxFileContent(fileContent);
-                }
-                else {
-                    console.error(`Failed to fetch content of ${filePath}:`, fileContentResponse.statusText);
+                if (filePath !== "README.md") // later create whitelist file
+                 {
+                    const fileContentResponse = await axios_1.default.get(`https://raw.githubusercontent.com/skarthikeyan96/ga-hashnode-publish/${commitHash}/${customBlogPath}${filePath}`);
+                    if (fileContentResponse.status === 200) {
+                        const fileContent = fileContentResponse.data;
+                        console.log("fileContent", fileContent);
+                        parseMdxFileContent(fileContent);
+                    }
+                    else {
+                        console.error(`Failed to fetch content of ${filePath}:`, fileContentResponse.statusText);
+                    }
                 }
             }
         }
--- src/index.ts
+++ src/index.ts
@@ -47,20 +47,24 @@ const run = async () => {
         const filePath = file.filename;
 
         console.log("filePath", filePath);
-        const fileContentResponse = await axios.get(
-          `https://raw.githubusercontent.com/skarthikeyan96/ga-hashnode-publish/${commitHash}/${customBlogPath}${filePath}`
-        );
-
-        if (fileContentResponse.status === 200) {
-          const fileContent = fileContentResponse.data;
-          console.log("fileContent", fileContent)
-          parseMdxFileContent(fileContent);
-        } else {
-          console.error(
-            `Failed to fetch content of ${filePath}:`,
-            fileContentResponse.statusText
+        if(filePath !== "README.md") // later create whitelist file
+        {
+          const fileContentResponse = await axios.get(
+            `https://raw.githubusercontent.com/skarthikeyan96/ga-hashnode-publish/${commitHash}/${customBlogPath}${filePath}`
           );
+  
+          if (fileContentResponse.status === 200) {
+            const fileContent = fileContentResponse.data;
+            console.log("fileContent", fileContent)
+            parseMdxFileContent(fileContent);
+          } else {
+            console.error(
+              `Failed to fetch content of ${filePath}:`,
+              fileContentResponse.statusText
+            );
+          }
         }
+
       }
     } else {
       console.error(
