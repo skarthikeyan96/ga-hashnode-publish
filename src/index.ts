976fbfa4bf85d34bb34b4b2ec19f088bf47908c2
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2024-02-05 01:31:16 +0530

    cleaning up

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { getInput, setFailed } from "@actions/core";
 import { execSync } from "child_process";
 import { context, getOctokit } from "@actions/github";
+import axios from "axios";
 
 const hashnode_personal_access_token = getInput(
   "hashnode-personal-access-token"
@@ -8,7 +9,7 @@ const hashnode_personal_access_token = getInput(
 const hashnode_publication_id = getInput("hashnode-publication-id");
 const blog_custom_dir = getInput("blog-custom-dir");
 
-const run = () => {
+const run = async () => {
   console.log("hello world")
   if(!hashnode_personal_access_token){
     setFailed("Please add your hashnode personal access token")
@@ -22,7 +23,12 @@ const run = () => {
   const commitHash = execSync("git rev-parse HEAD").toString().trim();
 
   try {
-    console.log("payload ->", context?.payload?.pull_request?.commits_url)
+    const username = context?.payload?.pull_request?.user?.login;
+    const reponame = context?.payload?.repository?.name
+    const commitResponse = await axios.get(
+      `https://api.github.com/repos/${username}/${reponame}/commits/${commitHash}`
+    );
+    console.log("commit response", commitResponse)
   } catch (error) {
     console.log("error", error)
   }
